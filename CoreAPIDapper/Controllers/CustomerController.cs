9b317f60d74952562daa5c4f8013def757e6b10d
Jayant Kumar Tripathy <jayant@corp.csmpl.com>
2020-04-09 17:23:59 +0530

    Dapper Crud in ASP .NET Core API in SQL Server

---
+++ CoreAPIDapper/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreAPIDapper.Repository;
+using CoreAPIDapper.Models;
+namespace CoreAPIDapper.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRep;
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRep = customerRepository;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<List<Customer>> GetCustomers()
+        {
+            return await _customerRep.GetCustomers();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetByCustomerByID(int id)
+        {
+            return await _customerRep.GetCustomerByID(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Customer>> AddEditCustomer([FromBody]Customer customer)
+        {
+            if (customer == null || !ModelState.IsValid)
+            {
+                return BadRequest("Invalid State");
+            }
+
+            return await _customerRep.ADDEditCustomer(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteById(int id)
+        {
+            return await _customerRep.DeleteCustomer(id);
+        }
+    }
+}
---
+++ CoreAPIDapper/Repository/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAPIDapper.Models;
+namespace CoreAPIDapper.Repository
+{
+   public interface ICustomerRepository
+    {
+        Task<List<Customer>> GetCustomers();
+        Task<Customer> GetCustomerByID(int id);
+        Task<Customer> ADDEditCustomer(Customer customer);
+        Task<Customer> DeleteCustomer(int id);
+    }
+}
